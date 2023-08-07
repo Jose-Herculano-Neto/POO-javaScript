fc7859b0de7da83171bea1acd4d3107de13320cd
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-07 08:47:27 -0300

    animal

--- poo.js
+++ poo.js
@@ -1,3 +1,30 @@
+/*Crie uma classe chamada "Pessoa" que tenha os atributos "nome" e "idade". 
+Em seguida, crie um objeto chamado "pessoa1" e atribua a ele um nome e uma idade.
+
+
+* Crie uma classe chamada "Aluno" que herde da classe "Pessoa" (do exercício 1).
+A classe "Aluno" deve ter um atributo adicional chamado "matricula".
+Crie um objeto chamado "aluno1" que seja um aluno e imprima no console.
+
+* Modifique a classe "Pessoa" para que o atributo "idade" seja privado.
+Crie métodos getters e setters para acessar e alterar o valor da idade, respeitando o encapsulamento.
+
+* Crie uma classe chamada "Animal" com um método chamado "falar".
+Crie classes específicas que herdem de "Animal" (por exemplo: "Cachorro", "Gato", etc.)
+e sobrescrevam o método "falar" para exibir o som característico de cada animal.
+*/
+
+class animal {
+  constructor(tipo, falar) {
+    this.tipo = tipo;
+    this.falar = falar;
+  }
+}
+
+const animal1 = new animal("cachorro", "au au");
+console.log(animal1);
+
+/*
 class conta {
   constructor(titular) {
     this.numero = "001";
@@ -5,12 +32,14 @@ class conta {
     this.titular = titular;
   }
 }
+*/
 
+/*
 const conta1 = new conta("Eu");
 console.log(conta1)
 const conta2 = new conta();
-
-class Veiculo{
+*/
+/*class Veiculo{
   constructor(rodas, marca, cor, valor, escada){
     this.rodas = rodas;
     this.marca = marca;
@@ -30,7 +59,11 @@ class Pessoa{
   this.idade = idade;
   }
 }
-
-const pessoa1 = new Pessoa("Zé", 33);
+*/
+/*const pessoa1 = new Pessoa("Zé", 33);
 
 console.log(pessoa1)
+*/
+/* Na classe "Pessoa" criada anteriormente, adicione um método chamado "apresentar" que exiba no console
+uma mensagem com o nome e a idade da pessoa.
+*/
